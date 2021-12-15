980accc88d479460d167c184eab68d454185151d
Guilherme Perroni <guilhermegasparperroni97@gmail.com>
2021-12-14 22:37:58 -0300

    Adicionado os métodos: Depositar, Sacar e Transferir na classe Conta; Adicionado a classe Cliente e modificado o tipo de titular da classe Conta de String para Cliente.

--- src/com/ggperroni/Conta.java
+++ src/com/ggperroni/Conta.java
@@ -4,5 +4,28 @@ public class Conta {
     double saldo;
     int agencia;
     int numero;
-    String titular;
+    Cliente titular;
+
+    public void depositar(double valor) {
+        System.out.println("Foi depositado R$" + valor + " na sua conta");
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if(this.saldo < 0) {
+            System.out.println("Saldo insuficiente!");
+            return false;
+        }
+        this.saldo -= valor;
+        return true;
+    }
+
+    public boolean transferir(double valor, Conta destino) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
 }
--- src/com/ggperroni/Main.java
+++ src/com/ggperroni/Main.java
@@ -4,7 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         Conta conta = new Conta();
-        conta.saldo = 85.578;
-        System.out.println(conta.saldo);
     }
 }
